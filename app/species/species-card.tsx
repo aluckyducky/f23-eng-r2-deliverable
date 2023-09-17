1b71979edc2fde71bfc2f94d321d95be21540ae5
Alice <aliceliu@college.harvard.edu>
2023-09-16 20:47:23 -0400

    Run prettier

--- app/species/species-card.tsx
+++ app/species/species-card.tsx
@@ -1,22 +1,21 @@
-"use client"
+"use client";
 
 import { Button } from "@/components/ui/button";
-import type { Database } from "@/lib/schema";
-import Image from "next/image";
 import {
   Dialog,
-  DialogContent,
   // DialogDescription,
   // DialogHeader,
   // DialogTitle,
   DialogClose,
+  DialogContent,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import type { Database } from "@/lib/schema";
+import Image from "next/image";
 
 import { X } from "lucide-react";
 
-import { useState} from "react";
-
+import { useState } from "react";
 
 type Species = Database["public"]["Tables"]["species"]["Row"];
 
@@ -39,28 +38,29 @@ export default function SpeciesCard(species: Species) {
       <p>{species.description ? species.description.slice(0, 150).trim() + "..." : ""}</p>
       {/* Replace with detailed view */}
       <Dialog open={open} onOpenChange={setOpen}>
-      <DialogTrigger asChild>
-        <Button className="mt-3 w-full" onClick={() => setOpen(true)}>Learn More</Button>
-      </DialogTrigger>
-      <DialogContent className="max-h-screen overflow-y-auto sm:max-w-[600px]">
-      <div style={{ display: 'flex', marginTop:-10, justifyContent: 'flex-end' }}>
-      <DialogClose >
-          <button className="" aria-label="Close">
-            <X />
-          </button>
-        </DialogClose>
-      </div>
-      {/* Stretch goal - turn into two columns, with most info on left and description on right */}
-      <h3 className="mt-2 text-2xl font-semibold">{species.common_name}</h3>
-      <h4 className="text-lg font-light italic">{species.scientific_name}</h4>
-      <p className="text-sm -my-1">Kingdom: {species.kingdom}</p>
-      <p className="text-sm -my-1">Total Population: {species.total_population}</p>
-      <p>{species.description}</p>
-        
-      </DialogContent>
-    </Dialog>
-
-      
+        <DialogTrigger asChild>
+          <Button className="mt-3 w-full" onClick={() => setOpen(true)}>
+            Learn More
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="max-h-screen overflow-y-auto sm:max-w-[600px]">
+          <div style={{ display: "flex", marginTop: -10, justifyContent: "flex-end" }}>
+            <DialogClose>
+              <button className="" aria-label="Close">
+                <X />
+              </button>
+            </DialogClose>
+          </div>
+          {/* Stretch goal - turn into two columns, with most info on left and description on right */}
+          <h3 className="mt-2 text-2xl font-semibold">{species.common_name}</h3>
+          <h4 className="text-lg font-light italic">{species.scientific_name}</h4>
+          <p className="-my-1 text-sm">Kingdom: {species.kingdom}</p>
+          <p className="-my-1 text-sm">
+            Total Population: {species.total_population ? species.total_population : "N/A"}
+          </p>
+          <p>{species.description}</p>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
